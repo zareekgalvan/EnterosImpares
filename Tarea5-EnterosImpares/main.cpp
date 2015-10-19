eb557c0c30e8fbaba3475f2b61c37c387db79a3a
Zareek Galván <zareek99@gmail.com>
2015-10-19 08:32:20 -0500

    First commit

--- Tarea5-EnterosImpares/main.cpp
+++ Tarea5-EnterosImpares/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int subSumImpairs(int arr[], int intsize)
+{
+    int intAcum = 0;
+    for (int i = 0; i < intsize; i++)
+    {
+        if (arr[i]%2 != 0)
+        {
+            intAcum += arr[i];
+        }
+    }
+    return intAcum;
+}
+
+int subSumImpairsRecursive(int arr[], int intsize)
+{
+    intsize -= 1;
+    if (intsize < 0)
+    {
+        return 0;
+    }
+    return (arr[intsize]%2!=0 ? arr[intsize] : 0) + subSumImpairsRecursive(arr, intsize);
+}
+
+int main()
+{
+    int arrintNumbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,15,14};
+    cout << sizeof(arrintNumbers)/sizeof(arrintNumbers[0]) << endl;
+    
+    cout << subSumImpairs(arrintNumbers, sizeof(arrintNumbers)/sizeof(arrintNumbers[0])) << endl;
+    cout << subSumImpairsRecursive(arrintNumbers, sizeof(arrintNumbers)/sizeof(arrintNumbers[0])) << endl;
 }
